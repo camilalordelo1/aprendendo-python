b55d09cad3cdb5b6e8204e1d9c7bd663331ea096
Camila Lordelo <camilalordelo15@gmail.com>
2023-09-03 17:25:34 -0300

    exe005

---
+++ sequencial/exe005.py
@@ -0,0 +1,7 @@
+# Faça um Programa que converta metros para centímetros.
+
+metro = input("Digite uma quantidade de metros: ")
+
+mParaCm = int(metro) * 100
+
+print(f'{metro} metros eh equivalente a {mParaCm} centimetros')
